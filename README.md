aebec3283737648c6aa5e078549bfe11232727aa
Hisham Amery <hishamamery@gmail.com>
2020-06-28 17:53:23 -0600

    Close to final application. App is working properly, still checking the conditional if statements prompting the user for input

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# Random-Password-Generator
 Random Password Generator
+
+During this project I focused on creating an easy to use random password generator. The project prompts the user for the inputs
+they would like in their password and returns a randomized final password. The user must chose a password legnth that is at least 8 
+and no more than 128 characters. 
+
+Author: Hisham Amery
+
+Technologies/Languages: HTML, CSS, JavaScript
+
+Screenshot Links:
+
+Final Product: https://ibb.co/W616dsw
+
+
--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // Arrays Being Used to Generate Password
 
-
 var number = "1234567890".split("")
 var lowercase = "abcdefghijklmnopqrstuvwxyz".split("")
 var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
@@ -9,20 +8,18 @@ var symbol = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~".split("")
 
 // Parseint converts string to number
 
+// The below function has all info for pass options
 
 function getPasswordOptions() {
 
   var passwordLength = parseInt(prompt("How many characters would you like your password to be?"));
 
-
-
   // This if statement makes sure the password length is between 8-128 characters 
 
   if (passwordLength < 8 || passwordLength > 128) {
-
     passwordLength = prompt(" How many characters would you like your password to be? Must be 8-128 characters");
   }
-
+ 
   // Checks for type of inputs user would like to include in password. Used new conditional variables
 
   var conNumber = confirm("Would you like to include numbers?");
@@ -30,89 +27,91 @@ function getPasswordOptions() {
   var conUppercase = confirm("Would you like to include uppercase letters?");
   var conSymbol = confirm("Include symbols?");
 
-  
   // If  user decides to answer "no" on all the questions, we send this alert, then ask again
 
   if (!(conNumber || conLowercase || conUppercase || conSymbol)) {
     alert("You must select at least one input type!");
-   
-  }
-  var passwordOptions = {
 
-    // Object w/ key value pairs. Makes it easy to pass info from one function to another
+  }
+
+  // Object w/ key value pairs
+
+  var passwordOptions = {
 
     passwordLength: passwordLength,
     conNumber: conNumber,
     conLowercase: conLowercase,
     conUppercase: conUppercase,
     conSymbol: conSymbol
+
   }
 
-return passwordOptions //the return makes this object accessibile later 
+  return passwordOptions //the return makes this object accessible later 
 
 }
 
+//Generates random array using Math.random() function
+
 function getRandom(Array) {
 
-  var randomIndex = Math.floor(Math.random()*Array.length) //need to make a random index
-  var randomElement= Array[randomIndex] //random elenet is passing through random index 
+  var randomIndex = Math.floor(Math.random() * Array.length)
+  var randomElement = Array[randomIndex]
   return randomElement
 
 }
 
-function generatePassword () {
+function generatePassword() {
 
-  var options = getPasswordOptions () //function call... that obkjkect is now called options not get pass...
-  var result = [] //empty array is used to store stuff
+  var options = getPasswordOptions()
+  var result = []
   var possibleChars = []
   var gauaranteedChars = []
 
-
-//these if statement are checking if used confirmed charc, numbers ect
+  //these if statement are checking what parameters users chose and then randomizing them as needed
 
   if (options.conSymbol) {
 
-    possibleChars = possibleChars.concat (symbol)
-    gauaranteedChars.push (getRandom(symbol))    //push takes that symbol and pushes it into empty array
+    possibleChars = possibleChars.concat(symbol)
+    gauaranteedChars.push(getRandom(symbol)) //push takes symbol and pushes it into empty array above
 
-  }   
+  }
 
   if (options.conNumber) {
 
-    possibleChars = possibleChars.concat (number)
-    gauaranteedChars.push (getRandom(number))    //push takes that symbol and pushes it into empty array
+    possibleChars = possibleChars.concat(number)
+    gauaranteedChars.push(getRandom(number))
 
-  }   
+  }
 
   if (options.conUppercase) {
 
-    possibleChars = possibleChars.concat (uppercase)
-    gauaranteedChars.push (getRandom(uppercase))    //push takes that symbol and pushes it into empty array
+    possibleChars = possibleChars.concat(uppercase)
+    gauaranteedChars.push(getRandom(uppercase))
 
-  } 
-  
+  }
+
   if (options.conLowercase) {
 
-    possibleChars = possibleChars.concat (lowercase)
-    gauaranteedChars.push (getRandom(lowercase))    //push takes that symbol and pushes it into empty array
+    possibleChars = possibleChars.concat(lowercase)
+    gauaranteedChars.push(getRandom(lowercase)) //push takes that symbol and pushes it into empty array
 
-  }   
+  }
 
-for (i=0; i<options.passwordLength; i++) { //looking at options object and getting key value pair password length
+  for (i = 0; i < options.passwordLength; i++) { //looking at options object and getting key value pair password length
 
-  var possibleChar=getRandom(possibleChars)
+    var possibleChar = getRandom(possibleChars)
 
-  result.push (possibleChar)
+    result.push(possibleChar)
 
-}
+  }
 
-for (i=0; i<gauaranteedChars.length; i++) { //looking at array of gauarnteed chars and counting how long it is
+  for (i = 0; i < gauaranteedChars.length; i++) { //looking at array of gauaranteed chars and counting how long it is
 
-  result[i]=gauaranteedChars[i]
+    result[i] = gauaranteedChars[i]
 
-}
+  }
 
-return result.join("")   //join is the opposite of split 
+  return result.join("") //join is the opposite of split 
 
 }
 
